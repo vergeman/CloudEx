7e8c9b04ff983cc6cfefd1aa8410043949004d4a
scanaran <scanaran@gmail.com>
2011-12-04 14:49:03 -0500

    lets see again

---
+++ src/edu/columbia/e6998/cloudexchange/client/AccountServlet.java
@@ -0,0 +1,59 @@
+package edu.columbia.e6998.cloudexchange.client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+@SuppressWarnings("serial")
+public class AccountServlet extends HttpServlet {
+
+	final String destination = "/views/account.jsp";
+
+	@SuppressWarnings("unchecked")
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/serve");
+
+		String fileName = "random file name";
+	
+		req.setAttribute("fileName", fileName);
+
+		rd = getServletContext().getRequestDispatcher(destination);
+		try {
+			rd.forward(req, resp);
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
+
