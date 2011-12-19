c8ff64415fe2a624b6b6453699eeb89ecbc6547b
Evgeny Fedotov <ef2363@columbia.edu>
2011-12-19 08:22:58 -0500

    added PositionEntry

---
+++ src/edu/columbia/e6998/cloudexchange/client/PositionEntry.java
@@ -0,0 +1,20 @@
+package edu.columbia.e6998.cloudexchange.client;
+
+import java.util.Date;
+
+public class PositionEntry {
+
+	public String buyOrSell;
+	public Date date;
+	public String ami;
+	public String instance;
+	public String region;
+	public String zone;
+	public String contractPrice;
+	public String bidAskPrice;
+	public String spotPrice;
+	
+	public PositionEntry () {
+		
+	}
+}
